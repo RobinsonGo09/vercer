800bd76a481d6b9409858cb9a7ea05f146f14139
Robinsongo09 <eduinnovaciongomez@gmail.com>
2026-03-06 07:26:51 -0500

    Cambios

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 function calcularPromedio(){
 
-    let n1 = parseFloat(prompt("Ingrese el primer número"));
-    let n2 = parseFloat(prompt("Ingrese el segundo número"));
-    let n3 = parseFloat(prompt("Ingrese el tercer número"));
+let n1 = parseFloat(prompt("Ingrese el primer número"));
+let n2 = parseFloat(prompt("Ingrese el segundo número"));
+let n3 = parseFloat(prompt("Ingrese el tercer número"));
 
-    let suma = n1 + n2 + n3;
+if(isNaN(n1) || isNaN(n2) || isNaN(n3)){
+document.getElementById("resultado").innerHTML = "Ingrese números válidos";
+return;
+}
 
-    let promedio = suma / 3;
+let suma = n1 + n2 + n3;
 
-    let resultado = document.getElementById("resultado");
+let promedio = suma / 3;
 
-    resultado.innerHTML = "El promedio es: " + promedio;
+document.getElementById("resultado").innerHTML =
+"El promedio es: " + promedio.toFixed(2);
 
 }
